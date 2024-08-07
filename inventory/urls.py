850fad3231376f0c815c4f94e50bea3c6ae4cd80
kiannaquines <kjgnaquines@gmail.com>
2024-08-07 12:49:32 +0800

    changes in sidebar

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from inventory.views import DashboardPage, EquipmentList, UserList, ChemicalList
+from inventory.views import *
 
 urlpatterns = [
     # Dashboard View
@@ -7,8 +7,13 @@ urlpatterns = [
     
     # List Views
     path('equipments/',EquipmentList.as_view(),name="equipments"),
+    path('equipments/category',EquipmentCategory.as_view(),name="equipment_category"),
+    path('equipments/report',EquipmentReport.as_view(),name="equipment_report"),
     path('users/',UserList.as_view(),name="users"),
     path('chemicals/',ChemicalList.as_view(),name="chemicals"),
+    path('chemicals/category',ChemicalList.as_view(),name="chemical_category"),
+    path('chemicals/report',ChemicalList.as_view(),name="chemical_report"),
+    
 
     # Create Views
 
