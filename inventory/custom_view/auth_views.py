75e7ba73dc4c38301e639f1361cf2e9bfebe191e
kiannaquines <kjgnaquines@gmail.com>
2024-08-08 20:38:56 +0800

    added get_context in register page and form_valid

--- inventory/custom_view/auth_views.py
+++ inventory/custom_view/auth_views.py
@@ -1,9 +1,12 @@
+from typing import Any
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView,CreateView
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User as DefaultUser
 from inventory.forms import RegisterForm
+from django.urls import reverse_lazy
+
 class LoginPage(LoginView):
     template_name = 'registration/login.html'
 
@@ -11,7 +14,17 @@ class RegisterPage(CreateView):
     template_name = 'registration/register.html'
     model = DefaultUser
     form_class = RegisterForm
+    success_url = reverse_lazy('inventory:login')
 
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    def form_valid(self, form: Any) -> HttpResponseRedirect:
+        response = super().form_valid(form)
+        return response
+    
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
